10bf950fc5ed42d29558129fa45e2ec9d81e898d
Иванъ <ivan@MacBook-Pro-Sofiya.local>
2023-11-16 23:26:31 +0300

    all done

---
+++ lab1/task3/src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.print("Гармонический ряд: ");
+
+        for (double i = 0; i < 10; i++) 
+        {
+            System.out.printf(" %.2f \n", 1/i );
+        }
+
+    
+    }
+}
