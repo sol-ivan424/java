963117371b99a40b35a94d417af80b2fdc9502ad
Иванъ <ivan@MacBook-Pro-Sofiya.local>
2023-11-16 03:44:46 +0300

    1 done

--- lab1/test/src/main/java/com/example/App.java
+++ lab1/test/src/main/java/com/example/App.java
@@ -6,12 +6,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        int[] myArray = {1, 2, 3, 4, 5};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        int size = sc.nextInt();
+        int[] myArray = new int[size];
 
-        // Вывод значений массива
+        for (int i = 0; i < size; i++)
+        {
+            System.out.println("Введите " + i + " елемент массива: ");
+            myArray[i] = sc.nextInt();
+        }
+        int summ = 0;
+
         for (int i = 0; i < myArray.length; i++) 
         {
-            System.out.println("Элемент " + i + ": " + myArray[i]);
+            summ += myArray[i];
         }
+        int sred = summ / myArray.length;
+        System.out.println("сумма элементов = " + summ);
+        System.out.println("среднее: " + sred);
+        sc.close();
     }
 }
